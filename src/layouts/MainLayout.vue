9155e8c68fe6f73019f0848611e3bd84c4e66b6a
jxthjwgpd <jxthjwgpd@outlook.com>
2021-08-02 20:38:56 +0800

    0.0.1 MainLayout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,7 +1,7 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
-      <q-toolbar>
+      <q-toolbar class="MAIN__toolbar">
         <q-btn
           flat
           dense
@@ -11,11 +11,30 @@
           aria-label="Menu"
         />
 
-        <q-toolbar-title>
-          Quasar Element Pro
+        <q-toolbar-title shrink class="row items-center no-wrap">
+          <img src="https://cdn.quasar.dev/img/layout-gallery/logo-google.svg">
+          <span class="q-ml-sm" v-if="$q.screen.gt.sm">Quasar Element Pro</span>
         </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <q-space />
+
+        <div class="q-gutter-sm row items-center no-wrap">
+          <q-btn round dense flat icon="apps" v-if="$q.screen.gt.sm">
+            <q-tooltip>Google Apps</q-tooltip>
+          </q-btn>
+          <q-btn round dense flat icon="notifications" v-if="$q.screen.gt.sm">
+            <q-badge color="red" text-color="white" floating>
+              2
+            </q-badge>
+            <q-tooltip>Notifications</q-tooltip>
+          </q-btn>
+          <q-btn round flat>
+            <q-avatar size="26px">
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+            </q-avatar>
+            <q-tooltip>Account</q-tooltip>
+          </q-btn>
+        </div>
       </q-toolbar>
     </q-header>
 
@@ -33,9 +52,38 @@
         />
       </q-list>
     </q-drawer>
+    <q-page-container class="MAIN__page">
+      <div class="MAIN__page-view-nav">
+        <div class="fit column">
+            <q-btn dense flat color="grey-8" no-caps size="26px" class="MAIN__side-btn">
+              <q-icon size="22px" name="photo" />
+              <q-tooltip anchor="center right" self="center left" transition-show="fade">Photo</q-tooltip>
+            </q-btn>
 
-    <q-page-container>
-      <router-view />
+            <q-btn dense flat color="grey-8" no-caps size="26px" class="MAIN__side-btn">
+              <q-icon size="22px" name="collections_bookmark" />
+              <q-tooltip anchor="center right" self="center left" transition-show="fade">Bookmark</q-tooltip>
+            </q-btn>
+
+            <q-btn dense flat color="grey-8" no-caps size="26px" class="MAIN__side-btn">
+              <q-icon size="22px" name="assistant" />
+              <q-tooltip anchor="center right" self="center left" transition-show="fade">Assistant</q-tooltip>
+            </q-btn>
+
+            <q-btn dense flat color="grey-8" no-caps size="26px" class="MAIN__side-btn">
+              <q-icon size="22px" name="group" />
+              <q-tooltip anchor="center right" self="center left" transition-show="fade">Group</q-tooltip>
+            </q-btn>
+
+            <q-btn dense flat color="grey-8" no-caps size="26px" class="MAIN__side-btn">
+              <q-icon size="22px" name="import_contacts" />
+              <q-tooltip anchor="center right" self="center left" transition-show="fade">Contacts</q-tooltip>
+            </q-btn>
+          </div>
+      </div>
+      <div class="MAIN__page-container">
+        <router-view class="router-view"/>
+      </div>
     </q-page-container>
   </q-layout>
 </template>
@@ -53,6 +101,8 @@ export default {
   data () {
     return {
       leftDrawerOpen: false,
+      search: '',
+      storage: 0.26,
       essentialLinks: [
         {
           title: 'Docs',
@@ -95,3 +145,39 @@ export default {
   }
 }
 </script>
+<style lang="sass" scoped>
+.MAIN
+  &__toolbar
+    height: 50px
+  &__side-btn
+    border-radius: 0
+    &__label
+      font-size: 12px
+      line-height: 18px
+      letter-spacing: .01785714em
+      font-weight: 500
+  &__page
+    flex: auto 1 1;
+    height: 100%;
+    display: flex;
+    overflow: hidden;
+    &-view-nav
+      background: #fff;
+      border-right: 1px solid rgba(black, 0.1);
+      position: relative;
+      z-index: 2;
+      display: flex;
+      flex-direction: column;
+      width: 56px;
+    &-container
+      flex: auto 1 1;
+      width: 0;
+      overflow-x: hidden;
+      overflow-y: auto;
+      display: flex;
+      flex-direction: column;
+.router-view
+  flex: 1;
+  height: 0;
+  overflow: hidden;
+</style>
