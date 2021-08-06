30eb0825f8ceb0d14a9cd2524fbe1802ca8940ac
jxthjwgpd <jxthjwgpd@outlook.com>
2021-08-06 20:55:05 +0800

    0.0.2 UserLayout

---
+++ src/layouts/UserLayout.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserLayout'
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,17 +1,21 @@
 
 const routes = [
   {
+    path: '/user',
+    component: () => import('layouts/UserLayout.vue'),
+    children: [
+      { path: '', redirect: { name: 'user-login' } },
+      { path: 'login', name: 'user-login', component: () => import('pages/Login.vue') }
+    ]
+  },
+  {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
+    meta: { auth: true },
     children: [
       { path: '', component: () => import('pages/Index.vue') },
       { path: 'events', component: () => import('pages/Events.vue') }
     ]
-  },
-  {
-    path: '*',
-    name: 'not-found',
-    component: () => import('pages/Error404.vue')
   }
 ]
 
